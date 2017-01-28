daf639a85f85cb644fd4a4029bbb89df4e442931
Sebastien Klasa <klasa.sebastien@gmail.com>
2017-01-29 00:35:19 +0100

    Add phpunit.phar, DatabaseTest (incomplete) and update README

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # WebEvenementiel
 
 Gestion d'évenements en ligne
+
+## Installation (développement)
+
+### Linux
+
+- Installer Apache, PHP et un SGBD
+
+### Windows
+
+- Installer Wamp (ou similaire)
+- Ajouter le dossier de PHP à la variable PATH ('C:/Wamp/bin/php/php7.0.10' par exemple)
+
+## Tests unitaires
+
+### Linux
+
+Executer `phpunit tests`
+
+### Windows
+
+Executer `phpunit.phar tests`
---
+++ src/Framework/Database.php
@@ -0,0 +1,50 @@
+<?php
+
+/* Database.php
+ * Anthony Vuillemin
+ */
+
+namespace WebEvent\Framework;
+
+/**
+ * Connexion avec une base de donnees, permet d'executer des requetes SQL
+ */
+class Database // TODO: Renommer en PgDatabase ou permettre l'utilisation d'autres SGBD (MySQL, ...)
+{
+    private $_link;
+
+    /**
+     * Constructeur par defaut
+     *
+     * @param      string  $host      Adresse
+     * @param      string  $username  Login
+     * @param      string  $password  Mot de passe
+     * @param      string  $db        Nom de la base de donnees
+     */
+    public function __construct($host, $username, $password, $db)
+    {
+        $this->link = pg_connect('host='.$host.' dbname='.$db.' user='.$username.' password='.$password)  
+        or die('Couldn\'t connect to database : '.pg_last_error());
+        
+    }
+
+    public function __destruct()
+    {
+        pg_close($this->link)
+        or die('Couldn\'t disconnect from database : '.pg_last_error());
+    }
+
+    /**
+     * Executer une requete SQL
+     *
+     * @param      string  $query  Requete
+     *
+     * @return     resource  Resultat
+     */
+    public function query($query)
+    {
+        $result = pg_query($query)
+        or die('Query failed : '.pg_last_error());
+        return $result;
+    }
+}
