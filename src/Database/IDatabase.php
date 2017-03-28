7f041f16c53b443589d7346a819b3b5b21815c02
Sebastien Klasa <klasa.sebastien@gmail.com>
2017-03-28 11:10:47 +0200

    [Back] Add IDatabase, MyDatabase and delete PgDatabase

---
+++ src/Database/IDatabase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WebEvents\Database;
+
+interface IDatabase {
+
+	/**
+	 * Executes a query on the database
+	 *
+	 * @param      string  $query  The query
+	 * @return     PDOStatement The response or false if there was an error
+	 */
+	public function query($query);
+}
---
+++ src/Database/MyDatabase.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace WebEvents\Database;
+
+require_once(__DIR__ . "/IDatabase.php");
+
+/**
+ * Connexion with a PostgreSQL database
+ */
+class MyDatabase implements IDatabase
+{
+    private $pdo;
+
+    /**
+     * @param      string  $host      Hostname
+     * @param      string  $username  Login
+     * @param      string  $password  Password
+     * @param      string  $db        Database name
+     */
+    public function __construct($host, $username, $password, $db)
+    {
+        $this->pdo = new \PDO('mysql:' . $host . '=localhost;dbname=' . $db, $username, $password);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function __destruct()
+    {
+        $this->pdo = null;
+    }
+
+    public function query($query)
+    {
+        return $this->pdo->query($query);
+    }
+}
