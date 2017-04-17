07f0f0046786c1762e590425f212f9a25d510e1a
Sebastien Klasa <klasa.sebastien@gmail.com>
2017-04-17 19:41:48 +0200

    [Front] New menu

--- web/index.php
+++ web/index.php
@@ -2,29 +2,48 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" lang="fr">
 <head>
-	
-	<?php include __DIR__ . "/parts/head.html"; ?>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/header.css"/>
+    <meta charset="utf-8">
+
+    <script src="./jquery.min.js"></script>
+    <script src="js/ajax.js"></script>
+
+    <title>Web Evenementiel</title>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#contents').load('pages/home.html');
+        });
+    </script>
 
 </head>
 <body>
 
-	<?php include __DIR__ . "/parts/header.html"; ?>
-	<?php include __DIR__ . "/parts/nav.html"; ?>
+<header>
+    <h1>Le web evenementiel</h1>
+    <nav>
+        <ul>
+            <li class="active">Accueil</li>
+            <li>Créer un événement</li>
+            <li>Mes événements</li>
+            <li>Mon compte</li>
+            <li>Nous contacter</li>
+        </ul>
+    </nav>
+    <div id="login">
+        <div class="login_helper">
+            <a id="signin_button" href=""><span>Connexion</span></a>
+            <br>
+            <a id="signup_button" href="create_profile.php"><span>Inscription</span></a>
+        </div>
+    </div>
+</header>
 	
-	<section class="contents">
-		<p><strong>Vous avez un évenement qui arrive prochainement et vous ne savez pas comment le mettre en place? Alors vous êtes au bon endroit !</strong></p>
-		<img id="eve" src="evenementiel.png" alt="Evenementiel Entreprise" title="Evenementiel Entreprise" />
-		<p>Notre société est organisatrice d'évenements sur Internet.
-		<p>Vous pourrez tout d'abord vous créer un profil. Ensuite, à l'aide de ce site web spécialisé, vous pourrez créer votre événement.
-		<p>Il vous sera demandé toutes les caractéristiques spécifiques de votre événement.
-		<p>Une fois cette étape terminée, vous entrerez la liste des mails de vos amis, et un mail leur sera envoyé automatiquement 
-		<p>avec l'ensemble des informations concernants l'événement.
-		<p>A partir de cette étape, nous gérerons tout, vous serez informé en temps réel des personnes qui participent ou ne participent 
-		<p>pas à votre événement grâce à un suivi sur le site.
-		</p>
-		<p><u>Une petite photo de notre équipe de développeurs:</u></p>
-		<img id="dvp" src="developpeur.jpg" alt="Developpeur Entreprise" title="Developpeur Entreprise" />
-	</section>
+<section id="contents">
+
+</section>
 
 </body>
 </html>		
